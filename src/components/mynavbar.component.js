ace851c19614b2ebebf484b3396a11118d301ebd
Andrew Njoo <adnjoo@gmail.com>
2021-07-15 21:24:49 -0700

    edit

--- src/components/mynavbar.component.js
+++ src/components/mynavbar.component.js
@@ -15,22 +15,6 @@ export default class Mynavbar extends Component {
                 <Nav.Link href="/user">Create User</Nav.Link>
                </Navbar.Collapse>
            </Navbar>
-            // <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-            //     <Link to="/" className="navbar-brand">ExcerTracker</Link>
-            //     <div className="collapse navbar-collapse">
-            //         <ul className="navbar-nav mr-auto">
-            //             <li className="navbar-item">
-            //                 <Link to="/" className="nav-link">Exercises</Link>
-            //             </li>
-            //             <li className="navbar-item">
-            //                 <Link to="/create" className="nav-link">Create Exercise Log</Link>
-            //             </li>
-            //             <li className="navbar-item">
-            //                 <Link to="/user" className="nav-link">Create User</Link>
-            //             </li>                       
-            //         </ul>
-            //     </div>
-            // </nav>
         )
     }
 }
